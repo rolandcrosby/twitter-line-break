f3d91f0e932147c002916248a4fe1653b2f405e0
Roland Crosby <roland@rolandcrosby.com>
2015-01-19 17:18:25 -0500

    fix line endings, heh

--- marco.js
+++ marco.js
@@ -1,13 +1,13 @@
 ﻿var tweets = document.getElementsByClassName("js-tweet-text");
 for (var i = 0; i < tweets.length; i++) {
-	var tweet = tweets[i];
-	if (tweet.textContent.indexOf("\n\n") != -1) {
-		for (var j = 0; j < tweet.childNodes.length; j++) {
-			var el = tweet.childNodes[j];
-			if ((el.nodeType == Node.TEXT_NODE) && (el.textContent.indexOf("\n\n") != -1)) {
-				var txt = el.textContent.replace(/\n{2,}/g, "\n\nWAIT FOR IT…WAIT FOR IT…\n\n");
-				el.textContent = txt;
-			}
-		}
+  var tweet = tweets[i];
+  if (tweet.textContent.indexOf("\n\n") != -1) {
+    for (var j = 0; j < tweet.childNodes.length; j++) {
+      var el = tweet.childNodes[j];
+      if ((el.nodeType == Node.TEXT_NODE) && (el.textContent.indexOf("\n\n") != -1)) {
+        var txt = el.textContent.replace(/\n{2,}/g, "\n\nWAIT FOR IT…WAIT FOR IT…\n\n");
+        el.textContent = txt;
+      }
     }
+  }
 }
